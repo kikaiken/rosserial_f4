5fa8697becb0a32db37f8aee229f6573551b9275
EATER-LETS\Eater <g.m.eater34@gmail.com>
2018-08-24 23:42:22 +0900

    購読テスト。turn_ledトピックがTrueだったらLEDが点灯、そうでなければ消灯する

--- Src/mainpp.cpp
+++ Src/mainpp.cpp
@@ -8,33 +8,57 @@
 #include "mainpp.h"
 #include "ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
+#include "ros/time.h"
+
+void TurnLED(const std_msgs::Bool &led){
+	if(led.data){
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	}else{
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	}
+}
 
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
+std_msgs::Bool status_msg;
+ros::Time last;
 ros::Publisher chatter("chatter", &str_msg);
+ros::Publisher status("status", &status_msg);
+ros::Subscriber<std_msgs::Bool> turn_led("turn_led", TurnLED );
 char hello[] = "Hello world!";
 
+
+
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
-  nh.getHardware()->flush();
+	nh.getHardware()->flush();
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-  nh.getHardware()->reset_rbuf();
+	nh.getHardware()->reset_rbuf();
 }
 
 void setup(void)
 {
-  nh.initNode();
-  nh.advertise(chatter);
+	nh.initNode();
+	nh.advertise(chatter);
+	nh.subscribe(turn_led);
+	nh.advertise(status);
+	last = nh.now();
 }
 
 void loop(void)
 {
-  str_msg.data = hello;
-  chatter.publish(&str_msg);
-  nh.spinOnce();
+	if(nh.now().toSec() - last.toSec() >= 1){
+		status_msg.data = true;
+		status.publish(&status_msg);
+		str_msg.data = hello;
+		chatter.publish(&str_msg);
+		last = nh.now();
+	}
 
-  HAL_Delay(1000);
+	nh.spinOnce();
+	HAL_Delay(1);
 }
 
